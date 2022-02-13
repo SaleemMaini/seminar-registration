dd63e053e6b47d3b7efa514106e3c3cc32fdaf97
Saleem Maini <33354178+SaleemMaini@users.noreply.github.com>
2022-02-13 18:43:54 +0200

    Finishing Step 3

--- src/components/steps/Step3.js
+++ src/components/steps/Step3.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { Fragment, useState, useContext, useEffect } from "react";
+import Input from "../UI/Input";
+import { StepVisibilityContext } from "../../store/StepVisibilityContextProvider";
 
 const Step3 = () => {
-  return (
-    <div>Step3</div>
-  )
-}
+  const [step3CheckBoxIsChecked, setStep3CheckBoxIsChecked] = useState(false);
+  const stepsCtx = useContext(StepVisibilityContext);
 
-export default Step3
+  const step3CheckBoxChangeHandler = (e) => {
+    setStep3CheckBoxIsChecked(e.target.checked);
+  };
+
+  useEffect(() => {
+    if (step3CheckBoxIsChecked) {
+      stepsCtx.context.setStepIsDoneTrue("step3");
+    } else {
+      stepsCtx.context.setStepIsDoneFalse("step3");
+    }
+  }, [step3CheckBoxIsChecked]);
+  return (
+    <Fragment>
+      <Input
+        input={{
+          id: "step3CheckBox",
+          type: "checkbox",
+          checked: step3CheckBoxIsChecked,
+          onChange: step3CheckBoxChangeHandler,
+        }}
+        label="Are you ready to rock? "
+      />
+    </Fragment>
+  );
+};
+
+export default Step3;
